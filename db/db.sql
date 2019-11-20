23b87a302580597807e98c8c4090339fed93d2d7
gb-heima <754263923@qq.com>
2019-11-20 13:58:07 +0800

    更新README.md目录结构 gblfy 2019/11/20

--- db/db.sql
+++ db/db.sql
@@ -1,20 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50519
-Source Host           : localhost:3306
-Source Database       : editor-markdown
-
-Target Server Type    : MYSQL
-Target Server Version : 50519
-File Encoding         : 65001
-
-Date: 2018-03-04 15:49:25
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
 -- Table structure for editor
 -- ----------------------------
@@ -26,7 +9,3 @@ CREATE TABLE `editor` (
   `create_time` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of editor
--- ----------------------------
